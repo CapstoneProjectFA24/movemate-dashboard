4546ef8f6fb594df9f3b21b45ed67e37f116cc2a
vinh <vinhntse2002@gmail.com>
2024-11-07 16:00:20 +0700

    feat(update-service): initial update service ui (#37)

--- app/(dashboard)/dashboard/services/(create-services)/[id]/page.tsx
+++ app/(dashboard)/dashboard/services/(create-services)/[id]/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { getServiceById } from "@/features/services/action/services";
+import { getTruckCategorys } from "@/features/services/action/truck-category";
+import ServiceDetailUpdateForm from "@/features/services/components/services-form/service-detail-update-form";
+import React from "react";
 
-const ServiceDetailPage = () => {
+const ServiceDetailPage = async ({ params }: { params: { id: string } }) => {
+  const [service, truckCategorys] = await Promise.all([
+    getServiceById(params.id),
+    getTruckCategorys(),
+  ]);
+
   return (
-    <div>ServiceDetailPage</div>
-  )
-}
+    <div>
+      <ServiceDetailUpdateForm
+        service={service.data}
+        truckCategorys={truckCategorys.data}
+      />
+    </div>
+  );
+};
 
-export default ServiceDetailPage
+export default ServiceDetailPage;
