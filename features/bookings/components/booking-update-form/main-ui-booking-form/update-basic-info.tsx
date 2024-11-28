2e6805a8b53c68a6169fad223d76331b5fef1fc7
vinh <vinhntse2002@gmail.com>
2024-11-29 02:11:07 +0700

    refactor(calendar): refactor calendar

--- features/bookings/components/booking-update-form/main-ui-booking-form/update-basic-info.tsx
+++ features/bookings/components/booking-update-form/main-ui-booking-form/update-basic-info.tsx
@@ -281,24 +281,24 @@ const UpdateBasicInfo = ({
                       locale={vi}
                       selected={bookingDate!}
                       className="w-full"
-                      classNames={{
-                        months: "w-full flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-                        month: "space-y-4 w-full",
-                        caption: "flex justify-center pt-1 relative items-center text-lg font-semibold",
-                        caption_label: "text-base font-medium",
-                        nav: "space-x-1 flex items-center",
-                        nav_button: "h-8 w-8 bg-transparent p-0 hover:opacity-70",
-                        nav_button_previous: "absolute left-1",
-                        nav_button_next: "absolute right-1",
-                        table: "w-full border-collapse space-y-1",
-                        head_row: "flex justify-between w-full",
-                        head_cell: "text-muted-foreground rounded-md w-12 font-medium text-sm",
-                        row: "flex w-full justify-between mt-2",
-                        cell: "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent",
-                        day: "h-10 w-10 p-0 font-normal aria-selected:opacity-100",
-                        day_range_end: "day-range-end",
-                        day_selected: "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-                      }}
+                      // classNames={{
+                      //   months: "w-full flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+                      //   month: "space-y-4 w-full",
+                      //   caption: "flex justify-center pt-1 relative items-center text-lg font-semibold",
+                      //   caption_label: "text-base font-medium",
+                      //   nav: "space-x-1 flex items-center",
+                      //   nav_button: "h-8 w-8 bg-transparent p-0 hover:opacity-70",
+                      //   nav_button_previous: "absolute left-1",
+                      //   nav_button_next: "absolute right-1",
+                      //   table: "w-full border-collapse space-y-1",
+                      //   head_row: "flex justify-between w-full",
+                      //   head_cell: "text-muted-foreground rounded-md w-12 font-medium text-sm",
+                      //   row: "flex w-full justify-between mt-2",
+                      //   cell: "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent",
+                      //   day: "h-10 w-10 p-0 font-normal aria-selected:opacity-100",
+                      //   day_range_end: "day-range-end",
+                      //   day_selected: "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+                      // }}
                       onSelect={handleDateSelect}
                       initialFocus
                       disabled={(date) => {
