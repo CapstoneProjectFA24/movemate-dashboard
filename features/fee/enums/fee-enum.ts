e437a796b3255d10aa73151863632051b02cc297
vinh <vinhntse2002@gmail.com>
2024-12-01 15:50:07 +0700

    feat(create-fee-page): init create fee page (#103)

--- features/fee/enums/fee-enum.ts
+++ features/fee/enums/fee-enum.ts
@@ -5,7 +5,7 @@ export enum FeeType {
   DRIVER = "DRIVER",
   REVIEWER = "REVIEWER",
   WEEKEND = "WEEKEND",
-  OUTSIDE = "OUTSIDE",
+  OUTSIDE = "OUTSIDE_BUSINESS_HOURS",
   HOLIDAY = "HOLIDAY",
 }
 
@@ -19,3 +19,14 @@ export const FeeTypeName = {
   [FeeType.OUTSIDE]: "Phí ngoại",
   [FeeType.HOLIDAY]: "Phí ngày lễ",
 };
+
+export const FeeLable = [
+  { value: FeeType.SYSTEM, label: "Phí hệ thống" },
+  { value: FeeType.PORTER, label: "Phí bốc vác" },
+  { value: FeeType.TRUCK, label: "Phí vận chuyển" },
+  { value: FeeType.DRIVER, label: "Phí vận chuyển + bốc vác" },
+  { value: FeeType.REVIEWER, label: "Phí đánh giá" },
+  { value: FeeType.WEEKEND, label: "Phí cuối tuần" },
+  { value: FeeType.OUTSIDE, label: "Phí ngoại" },
+  { value: FeeType.HOLIDAY, label: "Phí ngày lễ" },
+];
