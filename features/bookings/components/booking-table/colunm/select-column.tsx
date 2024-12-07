1787d6f43020ec13c7c6417701c6790a7a34a28b
vinh <vinhntse2002@gmail.com>
2024-12-07 23:52:03 +0700

    fix(booking): booking-order-column status fix (#142)

--- features/bookings/components/booking-table/colunm/select-column.tsx
+++ features/bookings/components/booking-table/colunm/select-column.tsx
@@ -4,26 +4,22 @@ import { Checkbox } from "@/components/ui/checkbox";
 
 export const selectColumn = {
   id: "select",
-  header: ({ table }: { table: Table<IBooking> }) => (
-    <Checkbox
-      checked={table.getIsAllPageRowsSelected()}
-      onCheckedChange={(value) => {
-        table.toggleAllPageRowsSelected(!!value);
-      }}
-      aria-label="Chọn tất cả"
-      className="translate-y-[2px] dark:text-white"
-    />
-  ),
-  cell: ({ row }: { row: Row<IBooking> }) => (
-    <Checkbox
-      checked={row.getIsSelected()}
-      onCheckedChange={(value) => {
-        row.toggleSelected(!!value);
-      }}
-      aria-label="Chọn dòng"
-      className="translate-y-[2px] dark:text-white"
-    />
-  ),
+
+  cell: ({ row }: { row: Row<IBooking> }) => {
+
+    const assignments = row.original.assignments;
+
+    return (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => {
+          row.toggleSelected(!!value);
+        }}
+        aria-label="Chọn dòng"
+        className="translate-y-[2px] dark:text-white"
+      />
+    );
+  },
   enableSorting: false,
   enableHiding: false,
 } as const;
