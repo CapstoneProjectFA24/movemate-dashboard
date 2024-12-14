78dee1942f23503d53b1494ee453e6d9b7bbdfc4
lehananh0606 <lehananh06@gmail.com>
2024-12-14 17:08:03 +0700

    fix(overflow-datatable): fix overflow datatable (#191)

--- app/(dashboard)/dashboard/fee/layout.tsx
+++ app/(dashboard)/dashboard/fee/layout.tsx
@@ -4,8 +4,8 @@ interface FeeLayoutProps {
 
 const FeeLayout = ({ children }: FeeLayoutProps) => {
   return (
-    <div className="bg-gray-100 dark:bg-muted/40 min-h-screen p-4 md:p-6 rounded-md flex flex-col flex-grow flex-shrink">
-      {children}
+    <div className="bg-gray-100 dark:bg-muted/40 min-h-screen p-4 md:p-6 flex flex-col">
+      <div className="overflow-auto flex-grow">{children}</div>
     </div>
   );
 };
